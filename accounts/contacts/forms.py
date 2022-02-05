03aea45ac1517a08c9389c61bde4cc115f63d92d
Fabricourt <mfalme2030@gmail.com>
2022-02-05 16:37:18 +0300

    'accounts'

--- accounts/contacts/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import *
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-       model=Contactus
-       fields = ('name', 'email', 'phone', 'header', 'message')
